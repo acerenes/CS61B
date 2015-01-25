60307e9648c2abb58fb56267f1e7bc6eb5a5fee3
Alice Tarng <atarng@airbears2-10-142-64-130.AirBears2.1918.Berkeley.EDU>
2015-01-24 19:08:23 -0800

    Planet.java Added instance variables and constructor

--- hw1/Planet.java
+++ hw1/Planet.java
@@ -1,3 +1,18 @@
 public class Planet {
+	public double x;
+	public double y;
+	public double xVelocity;
+	public double yVelocity;
+	public double mass;
+	public String img;
+
+	public Planet(double xpos, double ypos, double xVel, double yVel, double m, String image) {
+		x= xpos;
+		y= ypos;
+		xVelocity= xVel;
+		yVelocity= yVel;
+		mass= m;
+		img= image;
+	}
 
 }
