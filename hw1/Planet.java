3c364485f2adeee3a5fb1bb477ddecbf3117e9ee
Alice Tarng <atarng@airbears2-10-142-67-168.AirBears2.1918.Berkeley.EDU>
2015-01-24 22:37:12 -0800

    Planet.java- hopefully draws

--- hw1/Planet.java
+++ hw1/Planet.java
@@ -55,4 +55,11 @@ public class Planet {
 		}
 	}
 
+	public void draw() {
+		StdDraw.setCanvasSize(); // set window size to default 512x512 pixels
+		StdDraw.setPenRadius(); // set pen size to default .002
+		StdDraw.setPenColor(); // set pen color to default- black
+		StdDraw.point(x, y); // draw a point at (x,y)
+	}
+
 }
