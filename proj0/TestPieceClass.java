4cd0f94befa421edcd8a5ec8ff6fce0bc884f8a0
Alice Tarng <atarng@airbears2-10-142-185-76.AirBears2.1918.Berkeley.EDU>
2015-02-06 13:19:54 -0800

    TestPieceClass.java Got the structure of junit in

---
+++ proj0/TestPieceClass.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestPieceClass {
+
+	/* Test Constructor */
+	@Test
+	
+
+
+	public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestPieceClass.class);
+    }
+}
