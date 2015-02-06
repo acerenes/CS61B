5397a4f2e5adb6439c556f684a4fd67741084d4d
Alice Tarng <atarng@airbears2-10-142-185-76.AirBears2.1918.Berkeley.EDU>
2015-02-06 13:12:36 -0800

    Piece.java Declared all the stuff

--- proj0/Piece.java
+++ proj0/Piece.java
@@ -1,3 +1,14 @@
 public class Piece {
-	
+
+	/* Declaring instance variables */
+	private boolean element;
+	private Board board; 
+	private int xpos;
+	private int ypos;
+	private String class; 
+
+	/* Constructor for a Piece */
+	public Piece(boolean isFire, Board b, int x, int y, String type) {
+
+	}
 }
