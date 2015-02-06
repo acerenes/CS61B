d0ca03873f3e8d8ae41830a7382f1bbad4f4d893
Alice Tarng <atarng@airbears2-10-142-185-76.AirBears2.1918.Berkeley.EDU>
2015-02-06 13:03:14 -0800

    Board.java tbh not sure what is different

--- proj0/Board.java
+++ proj0/Board.java
@@ -1,6 +1,9 @@
 public class Board {
 
 	public static void main(String [] args) {
-		
+		StdDrawPlus.setPenColor(StdDrawPlus.Color GRAY)
+		// Drawing the Board???
+		for (int i=0, int j=0; i<=7,  )
+			StdDrawPlus.filledSquare()
 	}
 }
