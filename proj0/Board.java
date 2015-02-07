8fd877974bd1f833e1e81188064f5553d2354292
Alice Tarng <atarng@airbears2-10-142-67-101.AirBears2.1918.Berkeley.EDU>
2015-02-06 17:51:28 -0800

    Board.java trying to set up an empty board

--- proj0/Board.java
+++ proj0/Board.java
@@ -1,5 +1,23 @@
 public class Board {
 
+	public Board(boolean shouldBeEmpty) {
+		if (shouldBeEmpty) { // Display a blank 8x8 board
+			for (i==0; i<=7; i= i+1) { // Rows
+				for (j==0; j<=7; j= j+1) { // Columns
+					// Decide which color the square will be
+					if ((i%2==0 && j%2==0) || (i%2!=0 && j%2!=0)) {
+						StdDrawPlus.setPenColor(StdDrawPlus.GRAY);
+					else {
+						StdDrawPlus.setPenColor(StdDrawPlus.RED);
+					}
+
+					}
+				}
+			}
+
+		}
+	}
+
 	public static void main(String [] args) {
 		StdDrawPlus.setPenColor(StdDrawPlus.Color GRAY)
 		// Drawing the Board???
