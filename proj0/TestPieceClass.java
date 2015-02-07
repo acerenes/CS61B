1417ef65df199c833ed56d2bb42a90f117623e34
Alice Tarng <atarng@airbears2-10-142-67-101.AirBears2.1918.Berkeley.EDU>
2015-02-06 19:16:12 -0800

    TestPieceClass.java added tests for side

--- proj0/TestPieceClass.java
+++ proj0/TestPieceClass.java
@@ -5,13 +5,24 @@ public class TestPieceClass {
 
 	/* Test Constructor */
 	@Test
-	public void testConstructor_and_isFire() { 
-		Board b= new Board(true); // Empty board???? Non-static method so can't do class.thing
+	public void testConstructor_and_isFire() { // Can't really test Constructor by itself
+		Board b= new Board(true); // Empty board. I think this is like calling the constructor, sorta?
 		Piece pawn= new Piece(true, b, 0, 0, "pawn");
 		assertEquals(true, pawn.isFire());
 
 		Piece bomb= new Piece(false, b, 5, 5, "bomb");
 		assertEquals(false, bomb.isFire());
+	}
+
+	/* Test side */
+	@Test
+	public void testside() {
+		Board b= new Board(true);
+		Piece bomb= new Piece(false, b, 5, 5, "bomb");
+		assertEquals(1, bomb.side());
+
+		Piece pawn= new Piece(true, b, 0, 0, "pawn");
+		assertEquals(0, pawn.side());
 	}
 
 
