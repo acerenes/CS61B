b3821326c5e00ce2b6cb2f6840005f3e268fb0b6
Alice Tarng <atarng@airbears2-10-142-67-101.AirBears2.1918.Berkeley.EDU>
2015-02-06 18:29:40 -0800

    Board.java BLANK BOARD SHOWS UP WHEN JAVA BOARD NOW YAY

--- proj0/Board.java
+++ proj0/Board.java
@@ -2,26 +2,29 @@ public class Board {
 
 	public Board(boolean shouldBeEmpty) {
 		if (shouldBeEmpty) { // Display a blank 8x8 board
-			for (i==0; i<=7; i= i+1) { // Rows
-				for (j==0; j<=7; j= j+1) { // Columns
+			int scale= 8; // 8 because far side of 7 is 8
+			StdDrawPlus.setXscale(0, scale);
+			StdDrawPlus.setYscale(0, scale);
+			for (int i=0; i<=7; i= i+1) { // Rows
+				for (int j=0; j<=7; j= j+1) { // Columns
 					// Decide which color the square will be
 					if ((i%2==0 && j%2==0) || (i%2!=0 && j%2!=0)) {
 						StdDrawPlus.setPenColor(StdDrawPlus.GRAY);
+					}
 					else {
 						StdDrawPlus.setPenColor(StdDrawPlus.RED);
 					}
+					// Now actually fill in the Square color
+					StdDrawPlus.filledSquare(j+0.5, i+0.5, 0.5);
+					StdDrawPlus.setPenColor(StdDrawPlus.WHITE); 
 
 					}
 				}
 			}
 
 		}
-	}
 
 	public static void main(String [] args) {
-		StdDrawPlus.setPenColor(StdDrawPlus.Color GRAY)
-		// Drawing the Board???
-		for (int i=0, int j=0; i<=7,  )
-			StdDrawPlus.filledSquare()
+		Board board= new Board(true);
 	}
 }
