cf8252f88738e7eaeee9a31ed776ed26438b5696
Alice Tarng <atarng@Alices-MacBook-Pro.local>
2015-02-14 19:53:46 -0800

    MaxPlanet.java done

--- hw3/MaxPlanet.java
+++ hw3/MaxPlanet.java
@@ -1,5 +1,6 @@
 import java.util.Comparator;
 /* Import anything else you need here. */
+import java.util.Arrays; 
 
 /**
  * MaxPlanet.java
@@ -9,6 +10,8 @@ public class MaxPlanet {
 
     /** Returns the Planet with the maximum value according to Comparator c. */
     public static Planet maxPlanet(Planet[] planets, Comparator<Planet> c) {
-        // REPLACE THIS LINE WITH YOUR SOLUTION
+    	// According to the Java documentation, can put in a comparator after the array to do like a ~special sort~
+    	Arrays.sort(planets, c); 
+    	return planets[planets.length -1]; 
     }
 }
