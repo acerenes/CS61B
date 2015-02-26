a19c57dc739665da64bece6ecba293a1aef385b3
Alice Tarng <atarng@airbears2-10-142-185-29.AirBears2.1918.Berkeley.EDU>
2015-02-26 15:25:13 -0800

    TestClorus.java wrote test for choose

--- lab6/creatures/TestClorus.java
+++ lab6/creatures/TestClorus.java
@@ -44,6 +44,57 @@ public class TestClorus {
 		assertNotSame(c, baby_c); 
 	}
 
+	@Test
+	public void testChoose() {
+		Clorus c= new Clorus(2); 
+		HashMap<Direction, Occupant> surrounded = new HashMap<Direction, Occupant>(); 
+		surrounded.put(Direction.TOP, new Impassible());
+        surrounded.put(Direction.BOTTOM, new Impassible());
+        surrounded.put(Direction.LEFT, new Impassible());
+        surrounded.put(Direction.RIGHT, new Impassible());
+
+        Action actual = c.chooseAction(surrounded);
+        Action expected = new Action(Action.ActionType.STAY);
+
+        assertEquals(expected, actual);
+
+
+        Clorus c2 = new Clorus(2); 
+        HashMap<Direction, Occupant> surrounded2 = new HashMap<Direction, Occupant>(); 
+        surrounded2.put(Direction.TOP, new Impassible()); 
+        surrounded2.put(Direction.BOTTOM, new Impassible());
+        surrounded2.put(Direction.LEFT, new Empty()); 
+        surrounded2.put(Direction.RIGHT, new Plip()); 
+
+        Action actual2= c2.chooseAction(surrounded2);
+        Action expected2= new Action(Action.ActionType.ATTACK, Direction.RIGHT);
+        assertEquals(expected2, actual2);
+
+
+        Clorus c3 = new Clorus(2); 
+        HashMap<Direction, Occupant> surrounded3 = new HashMap<Direction, Occupant>(); 
+        surrounded3.put(Direction.TOP, new Impassible()); 
+        surrounded3.put(Direction.BOTTOM, new Impassible());
+        surrounded3.put(Direction.LEFT, new Impassible()); 
+        surrounded3.put(Direction.RIGHT, new Empty()); 
+
+        Action actual3= c3.chooseAction(surrounded3);
+        Action expected3= new Action(Action.ActionType.REPLICATE, Direction.RIGHT);
+        assertEquals(expected3, actual3);
+
+
+		Clorus c4 = new Clorus(0.5); 
+        HashMap<Direction, Occupant> surrounded4 = new HashMap<Direction, Occupant>(); 
+        surrounded4.put(Direction.TOP, new Impassible()); 
+        surrounded4.put(Direction.BOTTOM, new Impassible());
+        surrounded4.put(Direction.LEFT, new Impassible()); 
+        surrounded4.put(Direction.RIGHT, new Empty()); 
+
+        Action actual4= c4.chooseAction(surrounded4);
+        Action expected4= new Action(Action.ActionType.MOVE, Direction.RIGHT);
+        assertEquals(expected4, actual4);        
+	}
+
 
 	public static void main(String[] args) {
 		System.exit(jh61b.junit.textui.runClasses(TestClorus.class));
