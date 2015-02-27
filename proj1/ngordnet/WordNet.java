324194ffeee24136e5c44ee83f59f75029319417
Alice Tarng <atarng@airbears2-10-142-65-63.AirBears2.1918.Berkeley.EDU>
2015-02-27 02:59:26 -0800

    WordNet.java I tried to write the digraph

--- proj1/ngordnet/WordNet.java
+++ proj1/ngordnet/WordNet.java
@@ -8,6 +8,8 @@ public class WordNet {
 
 	Digraph digraph; 
 	int vertices; 
+	Set<String[]> synset; 
+	Set<Integer[]> hyponym;
 
 	/** Creates a WordNet using files from synsetFilename and hypernymFilename **/
 	public WordNet(String synsetFilename, String hyponymFilename) {
@@ -17,26 +19,33 @@ public class WordNet {
 		// The hypernym file also won't, because it maps every synset to like its 5 million more specific babies
 
 		// Okay, so I want to store all of the synsets into a set, right? How do that?
-		String[] synset_string = StdIn.readAllLines(synsetFilename); // Returns as array of strings
-        Set<String> synset = new HashSet(Arrays.asList(synset_string)); 
-
+		/*while (StdIn.isEmpty(synsetFilename)) {
+			String[] synset_string = StdIn.readLine(synsetFilename); // Returns as array of strings
+	        Set<String> synset = new HashSet(Arrays.asList(synset_string)); 
+	    }
+*/
+	    synset = new HashSet<String[]>(); 
+	    In synset_file = new In(synsetFilename); // At this point I'm just copying hw1
+	    while (!synset_file.isEmpty()) {
+	    	String[] synset_string = synset_file.readLine();
+	    	synset.add(synset_string); 
+	    }
 
 		vertices = synset.size(); // # of synsets should be # of vertices
 		digraph = new Digraph(vertices); // Creates new Digraph with hopefully right # vertices
 
 
-		// Now I have to analyze the hypononyms
-		// Should I store the hyponyms, or just go ahead and map them? I should prbly store them. 
-		int[] hyponym_array = StdIn.readAllInts(hyponymFilename);
-		Set<int> hyponym = new HashSet(Arrays.asList(hyponym_array));
-
-		// Iterate through the hyponym set, and start mapping
-		// A set is a collection, so I can use an iterator
-		Iterator<int> iter= hyponym.iterator(); 
+		hyponym = new HashSet<Integer[]>(); 
+		In hyponym_file = new In(hyponymFilename);
+		while (!hyponym_file.isEmpty()) {
+			Integer[] hyponym_array = hyponym_file.readLine();
+			hyponym.add(hyponym_array); 
+		}
+		// hyponym set made; Now iterate through and start mapping
+		Iterator<Integer[]> iter= hyponym.iterator(); 
 		while (iter.hasNext()) {
-			// But I'm thinking there's probably an array within a set
-			int[] hyp_relations = iter.next();
-			for (i = 1; i < hyp_relations.length; i = i + 1) {
+			Integer[] hyp_relations = iter.next();
+			for (int i = 1; i < hyp_relations.length; i = i + 1) {
 				// First is synset; rest are offspring
 				digraph.addEdge(hyp_relations[0], hyp_relations[i]);
 			}
