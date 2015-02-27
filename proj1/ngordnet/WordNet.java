16745286b9f73e314e663673b81e15d905987e32
Alice Tarng <atarng@airbears2-10-142-65-63.AirBears2.1918.Berkeley.EDU>
2015-02-27 02:02:12 -0800

    WordNet.java I think I got the digraph?

--- proj1/ngordnet/WordNet.java
+++ proj1/ngordnet/WordNet.java
@@ -1,9 +1,12 @@
 package ngordnet; 
+import java.util.*; 
 import edu.princeton.cs.algs4.Digraph;
+import java.util.HashMap;
+import java.util.Set; 
 
 public class WordNet {
 
-	Diagraph digraph; 
+	Digraph digraph; 
 	int vertices; 
 
 	/** Creates a WordNet using files from synsetFilename and hypernymFilename **/
@@ -14,12 +17,32 @@ public class WordNet {
 		// The hypernym file also won't, because it maps every synset to like its 5 million more specific babies
 
 		// Okay, so I want to store all of the synsets into a set, right? How do that?
-		String[] synset_string = StdDraw.readAllLines(synsetFilename); // Returns as array of strings
-        Set<String> synset = new HashSet(Arrays.asList(hyponyms)); 
+		String[] synset_string = StdIn.readAllLines(synsetFilename); // Returns as array of strings
+        Set<String> synset = new HashSet(Arrays.asList(synset_string)); 
+
 
 		vertices = synset.size(); // # of synsets should be # of vertices
-		diagraph = Digraph(vertices); // Creates new Digraph with hopefully right # vertices
-		
+		digraph = new Digraph(vertices); // Creates new Digraph with hopefully right # vertices
+
+
+		// Now I have to analyze the hypononyms
+		// Should I store the hyponyms, or just go ahead and map them? I should prbly store them. 
+		int[] hyponym_array = StdIn.readAllInts(hyponymFilename);
+		Set<int> hyponym = new HashSet(Arrays.asList(hyponym_array));
+
+		// Iterate through the hyponym set, and start mapping
+		// A set is a collection, so I can use an iterator
+		Iterator<int> iter= hyponym.iterator(); 
+		while (iter.hasNext()) {
+			// But I'm thinking there's probably an array within a set
+			int[] hyp_relations = iter.next();
+			for (i = 1; i < hyp_relations.length; i = i + 1) {
+				// First is synset; rest are offspring
+				digraph.addEdge(hyp_relations[0], hyp_relations[i]);
+			}
+		}
+		// I think that finishes making the Digraph, hopefully? 
+
 	}
 
 
