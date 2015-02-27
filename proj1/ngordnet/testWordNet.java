a2690248e193847fe4cf7ca20af35068d4489965
Alice Tarng <atarng@airbears2-10-142-65-63.AirBears2.1918.Berkeley.EDU>
2015-02-26 23:55:20 -0800

    TestWordNet.java I think I wrote all the tests for WordNet

--- proj1/ngordnet/testWordNet.java
+++ proj1/ngordnet/testWordNet.java
@@ -8,11 +8,31 @@ public class TestWordNet {
 
 
 	@Test 
-	public void givenTests() {
+	public void testisNoun() {
 		assertTrue(wn.isNoun("jump"));
 		assertTrue(wn.isNoun("leap"));
 		assertTrue(wn.isNoun("nasal_decongestant")); 
 	}
+
+    @Test
+    public void testnouns() {
+        Set<String> expected_nouns = new Set<String> {"augmentation", "nasal_decongestant", "change", "action", "actified", "antihistamine", "increase", "descent", "parachuting", "leap", "demotion", "jump"}; 
+        Set<String> actual_nouns = new Set<String>; 
+        for (String noun : wn.nouns()) {
+            actual_nouns.add(noun); 
+        }
+        assertEquals(expected_nouns, actual_nouns); 
+    }
+
+    @Test
+    public void testhyponyms() {
+        Set<String> expected_hyponyms = new Set<String> {"agumentation", "increase", "leap", "jump"}; 
+        Set<String> actual_hyponyms = new Set<String>; 
+        for (String hyponym : wn.hyponyms("increase")) {
+            actual_hyponyms.add(hyponym); 
+        }
+        assertEquals(expected_hyponyms, actual_hyponyms); 
+    }
 
 	public static void main(String[] args) {
 		WordNet wn = new WordNet("./wordnet/synsets11.txt", "./wordnet/hypernyms11.txt");
@@ -31,11 +51,11 @@ public class TestWordNet {
             leap
             demotion
             jump
-        */
+        
         System.out.println("All nouns:");
         for (String noun : wn.nouns()) {
             System.out.println(noun);
-        }
+        }*/
 
         /* The code below should print the following: 
             Hypnoyms of increase:
@@ -43,11 +63,11 @@ public class TestWordNet {
             increase
             leap
             jump
-        */
+        
         System.out.println("Hypnoyms of increase:");
         for (String noun : wn.hyponyms("increase")) {
             System.out.println(noun);
-        }    
+        }  */  
 
 		jh61b.junit.textui.runClasses(TestWordNet.class); 
 
