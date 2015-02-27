b747213b97403700a4cc3e79439cd14363e4d7a3
Alice Tarng <atarng@airbears2-10-142-65-63.AirBears2.1918.Berkeley.EDU>
2015-02-26 23:35:16 -0800

    testWordNet.java wait what I thought I renamed it

--- proj1/ngordnet/testWordNet.java
+++ proj1/ngordnet/testWordNet.java
@@ -1,1 +1,56 @@
 package ngordnet; 
+import org.junit.Test;
+import static org.junit.Assert.*; 
+
+public class TestWordNet {
+
+
+
+
+	@Test 
+	public void givenTests() {
+		assertTrue(wn.isNoun("jump"));
+		assertTrue(wn.isNoun("leap"));
+		assertTrue(wn.isNoun("nasal_decongestant")); 
+	}
+
+	public static void main(String[] args) {
+		WordNet wn = new WordNet("./wordnet/synsets11.txt", "./wordnet/hypernyms11.txt");
+
+        /* The code below should print the following: 
+            All nouns:
+            augmentation
+            nasal_decongestant
+            change
+            action
+            actifed
+            antihistamine
+            increase
+            descent
+            parachuting
+            leap
+            demotion
+            jump
+        */
+        System.out.println("All nouns:");
+        for (String noun : wn.nouns()) {
+            System.out.println(noun);
+        }
+
+        /* The code below should print the following: 
+            Hypnoyms of increase:
+            augmentation
+            increase
+            leap
+            jump
+        */
+        System.out.println("Hypnoyms of increase:");
+        for (String noun : wn.hyponyms("increase")) {
+            System.out.println(noun);
+        }    
+
+		jh61b.junit.textui.runClasses(TestWordNet.class); 
+
+
+	}
+}
