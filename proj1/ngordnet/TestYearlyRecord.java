15eeb271ec2734597e0ce755aff29da1ad03a923
Alice Tarng <atarng@airbears2-10-142-187-243.AirBears2.1918.Berkeley.EDU>
2015-03-02 14:08:05 -0800

    TestYearlyRecord.java wrote test for rank

--- proj1/ngordnet/TestYearlyRecord.java
+++ proj1/ngordnet/TestYearlyRecord.java
@@ -1,1 +1,29 @@
 package ngordnet;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestYearlyRecord {
+
+    public static final NINETYFIVE = 95;
+    public static final THREEFORTY = 340;
+    public static final ONEEIGHTONE = 181;
+
+    @Test
+    public void testrank() {
+        YearRecord yr = new YearlyRecord();
+        yr.put("quayside", NINETYFIVE);
+        yr.put("surrogate", THREEFORTY);
+        yr.put("merchantman", ONEEIGHTONE);
+        assertEquals(1, yr.rank("surrogate"));
+        assertEquals(3, yr.rank("quayside"));
+    }
+
+
+
+
+
+    public static void main (String[] args) {
+        jh61b.junit.textui.runClasses(TestYearlyRecord.class);
+    }
+}
