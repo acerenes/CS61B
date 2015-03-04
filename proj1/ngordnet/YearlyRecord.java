57251586fe7b820ede02ac3836b1bd9b805c97b4
Alice Tarng <atarng@airbears2-10-142-185-156.AirBears2.1918.Berkeley.EDU>
2015-03-04 12:43:02 -0800

    YearlyRecord style fix

--- proj1/ngordnet/YearlyRecord.java
+++ proj1/ngordnet/YearlyRecord.java
@@ -22,65 +22,65 @@ import java.util.ArrayList;
 
 public class YearlyRecord {
 
-    Map<String, Integer> count_map;
-    Map<String, Integer> rank_map; 
+    Map<String, Integer> countMap;
+    Map<String, Integer> rankMap; 
     int size;
 
     /* Creates a new empty YearlyRecord. */
     public YearlyRecord() {
-        count_map = new HashMap<String, Integer>();
-        rank_map = new HashMap<String, Integer>();
+        countMap = new HashMap<String, Integer>();
+        rankMap = new HashMap<String, Integer>();
         size = 0;
     }
 
     /* Creates a YearlyRecord using the given data. */
     public YearlyRecord(HashMap<String, Integer> otherCountMap) {
         // I think you're supposed to create a new object.
-        count_map = new HashMap<String, Integer>();
-        rank_map = new HashMap<String, Integer>();
+        countMap = new HashMap<String, Integer>();
+        rankMap = new HashMap<String, Integer>();
         Set<String> keys = otherCountMap.keySet();
-        // First construct count_map.
+        // First construct countMap.
         for (String key : keys) {
-            count_map.put(key, otherCountMap.get(key));
+            countMap.put(key, otherCountMap.get(key));
             size = size + 1;
         }
-        // Construct rank_map.
+        // Construct rankMap.
         for (String key : keys) {
             // Now figure out the rank.
             int rank = 1; 
             for (String key2 : keys) {
-                if (otherCountMap.get(key2) > otherCountMap.get(key)) {
+                Integer mapValue = otherCountMap.get(key);
+                Integer mapValue2 = otherCountMap.get(key2);
+                if (mapValue2 > mapValue) {
                     rank = rank + 1;
-                }
-                else if (otherCountMap.get(key2) == otherCountMap.get(key) && (key2.compareTo(key) > 0)) {
+                } else if (mapValue2 == mapValue && (key2.compareTo(key) > 0)) {
                     rank = rank + 1;
                 }
             }
             // Figured out rank - put in. 
-            rank_map.put(key, rank);
+            rankMap.put(key, rank);
         }
     }
 
     /* Records that word occured count times in this year. */
     public void put(String word, int count) {
-        count_map.put(word, count);
+        countMap.put(word, count);
         size = size + 1;
-        // Update rank_map as well. 
-        rank_map = new HashMap<String, Integer>();
-        Set<String> keys = count_map.keySet();
+        // Update rankMap as well. 
+        rankMap = new HashMap<String, Integer>();
+        Set<String> keys = countMap.keySet();
         for (String key : keys) {
             // Figure out the rank.
             int rank = 1; 
             for (String key2 : keys) {
-                if (count_map.get(key2) > count_map.get(key)) {
+                if (countMap.get(key2) > countMap.get(key)) {
                     rank = rank + 1;
-                }
-                else if (count_map.get(key2) == count_map.get(key) && (key2.compareTo(key) > 0)) {
+                } else if (countMap.get(key2) == countMap.get(key) && (key2.compareTo(key) > 0)) {
                     rank = rank + 1;
                 }
             }
             // Figured out rank - put in. 
-            rank_map.put(key, rank);
+            rankMap.put(key, rank);
             // This will work b/c map drops its reference - put in new value.
         }
     }
@@ -94,13 +94,13 @@ public class YearlyRecord {
 
     /* Returns # times word appeared in this year. */
     public int count(String word) {
-        return count_map.get(word);
+        return countMap.get(word);
     }
 
 
     /* Returns rank of word; most common word = 1. */
     public int rank(String word) {
-        return rank_map.get(word);
+        return rankMap.get(word);
     }
 
 
@@ -112,38 +112,38 @@ public class YearlyRecord {
             * Iterate through the keys. 
             * If the value = counter, put the key in the list. 
             * Update counter. */
-        List<String> ascending_words = new ArrayList<String>();
-        Map<String, Integer> rankedmap = this.rank_map;
+        List<String> ascendingWords = new ArrayList<String>();
+        Map<String, Integer> rankedmap = this.rankMap;
         Set<String> keys = rankedmap.keySet();
         int counter = this.size();
         while (counter >= 1) {
             for (String key : keys) {
                 if (counter == rankedmap.get(key)) {
-                    ascending_words.add(key);
+                    ascendingWords.add(key);
                 }
             }
             counter = counter - 1;
         }
-        return ascending_words;
+        return ascendingWords;
     }
 
 
     /* Returns all counts in ascending order of count. */
     public Collection<Number> counts() {
-        List<Number> ascending_count = new ArrayList<Number>();
-        Map<String, Integer> rankedmap = this.rank_map;
-        Map<String, Integer> countmap = this.count_map;
+        List<Number> ascendingCount = new ArrayList<Number>();
+        Map<String, Integer> rankedmap = this.rankMap;
+        Map<String, Integer> countmap = this.countMap;
         Set<String> keys = rankedmap.keySet();
         int counter = this.size();
         while (counter >= 1) {
             for (String key : keys) {
                 if (counter == rankedmap.get(key)) {
-                    ascending_count.add(countmap.get(key));
+                    ascendingCount.add(countmap.get(key));
                 }
             }
             counter = counter - 1;
         }
-        return ascending_count;
+        return ascendingCount;
     }
 
 
