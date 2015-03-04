2017a64db0060944dddaa296b3f2d30f3877d3c9
Alice Tarng <atarng@airbears2-10-142-187-172.AirBears2.1918.Berkeley.EDU>
2015-03-03 16:11:15 -0800

    NGramMap wrote constructor - just bring in stuff

--- proj1/ngordnet/NGramMap.java
+++ proj1/ngordnet/NGramMap.java
@@ -18,6 +18,31 @@ public class NGramMap {
                 String[] wordsarray = wordsstring.split("\t");
                 words.add(wordsarray);
             }
+        } catch (FileNotFoundException ex) {
+            System.out.println("The words file is not valid.");
+        }
+
+        counts = new HashSet<int[]>();
+        File countsfile = new File(countsFilename);
+        try {
+            Scanner countsscanned = new Scanner(countsfile);
+            while (countsscanned.hasNextLine()) {
+                String countsInts = countsscanned.nextLine();
+                String[] countsStringarray = countsInts.split(",");
+                int[] countsArray = new int[countsStringarray.length];
+                for (int i = 0; i < countsStringarray.length; i = i + 1) {
+                    try { // Have to create int from string[].
+                        int element = Integer.parseInt(countsStringarray[i]);
+                        // Then add to int[].
+                        countsArray[i] = element;
+                    } catch (NumberFormatException nf) {
+                        System.out.println("There is a problem in the counts file - an element is not an int.");
+                    }
+                }
+                counts.add(countsArray);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("The counts file is not valid.");
         }
 
 
