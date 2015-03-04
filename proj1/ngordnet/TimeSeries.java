f3a04749a0c05eee9bbc3521f11dc624402209e1
Alice Tarng <atarng@airbears2-10-142-185-156.AirBears2.1918.Berkeley.EDU>
2015-03-04 12:34:11 -0800

    TimeSeries style

--- proj1/ngordnet/TimeSeries.java
+++ proj1/ngordnet/TimeSeries.java
@@ -20,7 +20,9 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
 
     /* Creates a copy of ts, but only between startyear & endyear. Inclusive of both end points. */
     public TimeSeries(TimeSeries<T> ts, int startYear, int endYear) {
-        // Iterate through ts, get its keys. If its key is between the endpts, grab it+its value and copy it into the new guy.
+        /* Iterate through ts.
+            * Get its keys. 
+            * If key is btwn endpts, grab it+its value and copy into the new guy. */
         new TimeSeries<T>(); // Constructing new TimeSeries.
         Set<Integer> keys = ts.keySet();
         for (Integer key : keys) {
@@ -43,16 +45,17 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
 
     /* Returns the quotient of this time series divided by the relevant value in ts. 
         * If ts is missing a key in this time series, return an IllegalArgumentException. */
-    public TimeSeries<Double> dividedBy (TimeSeries<? extends Number> ts) {
+    public TimeSeries<Double> dividedBy(TimeSeries<? extends Number> ts) {
         // Iterate thrrough this. Get your value and their value, divide. Put into new TimeSeries. 
         TimeSeries<Double> quotient = new TimeSeries<Double>();
         Set<Integer> keys = this.keySet();
         for (Integer key : keys) {
             if (!ts.containsKey(key)) {
-                throw new IllegalArgumentException("In method dividedBy, the given argument is missing a key.");
+                throw new IllegalArgumentException("In dividedBy, given argument is missing a key.");
             }
             quotient.put(key, (this.get(key).doubleValue() / ts.get(key).doubleValue()));
-            // Gotta do this weird .doubleValue thing because apparently you can only do basic operations on primitive types or smth .___.
+            // Gotta do this weird .doubleValue thing.
+            // Apparently can only do basic operations on primitive types or smth .___.
         }
         return quotient;
     }
@@ -67,19 +70,17 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
         for (Integer key1 : keys1) {
             if (!ts.containsKey(key1)) {
                 sum1 = this.get(key1).doubleValue() + 0;
-            }
-            else {
+            } else {
                 sum1 = this.get(key1).doubleValue() + ts.get(key1).doubleValue();
             }
             sum.put(key1, sum1);
         }
-        // I think I'll do it 2x - once for this and once for ts, in case of keys that one has that the other doesn't. 
+        // Do it 2x - once for this and once for ts, in case of keys that one has that the other doesn't. 
         Set<Integer> keys2 = ts.keySet();
         for (Integer key2 : keys2) {
             if (!this.containsKey(key2)) {
                 sum2 = ts.get(key2).doubleValue() + 0;
-            }
-            else {
+            } else {
                 sum2 = ts.get(key2).doubleValue() + this.get(key2).doubleValue();
             }
             sum.put(key2, sum2);
@@ -91,7 +92,7 @@ public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
     /* Returns all years for this time series in any order. */
     public Collection<Number> years() {
         Collection<Number> years = new ArrayList<Number>();
-        // Thanks a million to Stackoverflow for getting me around the whole can't instantiate Collection interface thing.
+        // Thanks Stackoverflow for getting me around the can't instantiate Collection interface thing.
         for (Number key : this.keySet()) {
             years.add((Number) key);
         }
