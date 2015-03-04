e58bd29697fc289adfcc9a594b00821725209181
Alice Tarng <atarng@airbears2-10-142-185-156.AirBears2.1918.Berkeley.EDU>
2015-03-04 12:28:36 -0800

    WordNet fixed style

--- proj1/ngordnet/WordNet.java
+++ proj1/ngordnet/WordNet.java
@@ -52,18 +52,18 @@ public class WordNet {
             Scanner hyponymscanned = new Scanner(hyponymfile);
             while (hyponymscanned.hasNextLine()) {
                 String hyponymints = hyponymscanned.nextLine();
-                String[] hyponym_Sarray = hyponymints.split(",");
-                int[] hyponym_array = new int[hyponym_Sarray.length];
-                for (int i = 0; i < hyponym_Sarray.length; i = i + 1){
+                String[] hyponymSarray = hyponymints.split(",");
+                int[] hyponymArray = new int[hyponymSarray.length];
+                for (int i = 0; i < hyponymSarray.length; i = i + 1) {
                     try { // Create int from string[].
-                        int element = Integer.parseInt(hyponym_Sarray[i]);
+                        int element = Integer.parseInt(hyponymSarray[i]);
                         // Add to our int[].
-                        hyponym_array[i] = element;
+                        hyponymArray[i] = element;
                     } catch (NumberFormatException nf) {
-                        System.out.println("There is a problem in the hyponym file - an element is not an int.");
+                        System.out.println("Problem in hyponym file - an element is not an int.");
                     }       
                 }
-                hyponym.add(hyponym_array);
+                hyponym.add(hyponymArray);
             }
         } catch (FileNotFoundException ex) {
             System.out.println("The hyponym file is not valid.");
@@ -71,22 +71,22 @@ public class WordNet {
         // Hyponym set made; Now iterate through and start mapping.
         Iterator<int[]> iter = hyponym.iterator(); 
         while (iter.hasNext()) {
-            int[] hyp_relations = iter.next();
-            for (int i = 1; i < hyp_relations.length; i = i + 1) {
+            int[] hypRelations = iter.next();
+            for (int i = 1; i < hypRelations.length; i = i + 1) {
                 // First is synset; rest are offspring.
-                digraph.addEdge(hyp_relations[0], hyp_relations[i]);
+                digraph.addEdge(hypRelations[0], hypRelations[i]);
             }
         }
     }
 
     /* Returns true if noun is a word in some synset. */
     public boolean isNoun(String noun) {
-        Iterator<String[]> syn_iter = this.synset.iterator();
-        while (syn_iter.hasNext()) {
-            String[] syn_array = syn_iter.next();
-            String[] actual_words= syn_array[1].split(" ");
-            for (int i = 0; i < actual_words.length; i = i + 1) {
-                if (actual_words[i].equals(noun)) {
+        Iterator<String[]> synIter = this.synset.iterator();
+        while (synIter.hasNext()) {
+            String[] synArray = synIter.next();
+            String[] actualWords = synArray[1].split(" ");
+            for (int i = 0; i < actualWords.length; i = i + 1) {
+                if (actualWords[i].equals(noun)) {
                     return true; 
                 }
             }
@@ -96,17 +96,17 @@ public class WordNet {
 
     /* Returns set of all nouns. */
     public Set<String> nouns() {
-        Set<String> all_nouns; 
-        all_nouns = new HashSet<String>(); 
-        Iterator<String[]> syn_iter = this.synset.iterator();
-        while (syn_iter.hasNext()) {
-            String[] syn_array = syn_iter.next();
-            String[] words = syn_array[1].split(" ");
+        Set<String> allNouns; 
+        allNouns = new HashSet<String>(); 
+        Iterator<String[]> synIter = this.synset.iterator();
+        while (synIter.hasNext()) {
+            String[] synArray = synIter.next();
+            String[] words = synArray[1].split(" ");
             for (int i = 0; i < words.length; i = i + 1) {
-                all_nouns.add(words[i]);
+                allNouns.add(words[i]);
             }
         }
-        return all_nouns; 
+        return allNouns; 
     }
 
 
@@ -120,52 +120,52 @@ public class WordNet {
 
         // Haave to get children of children as well.
 
-        Set<String> all_hyponyms = new HashSet<String>();
-        Iterator<String[]> syn_iter = this.synset.iterator();
+        Set<String> allHyponyms = new HashSet<String>();
+        Iterator<String[]> synIter = this.synset.iterator();
         // Figure out where the word occurs.
-        while (syn_iter.hasNext()) {
-            String[] syn_array = syn_iter.next();
-            String[] words = syn_array[1].split(" "); 
+        while (synIter.hasNext()) {
+            String[] synArray = synIter.next();
+            String[] words = synArray[1].split(" "); 
             for (int i = 0; i < words.length; i = i + 1) {
                 if (words[i].equals(word)) {
                     // Get the synset ID.
-                    int syn_ID = Integer.parseInt(syn_array[0]); 
+                    int synID = Integer.parseInt(synArray[0]); 
                     // Get self + synonyms
-                    Set<String> words2 = this.words(syn_ID);
-                    for (String add_word : words2) {
-                        all_hyponyms.add(add_word);
+                    Set<String> words2 = this.words(synID);
+                    for (String addWord : words2) {
+                        allHyponyms.add(addWord);
                     }
 
                     // Then find ALL YO DESCENDENTS, get their words, and store. 
-                    Set<Integer> all_children_IDs = this.allSynIDs(syn_ID);
-                    for (int syn_ids : all_children_IDs) {
-                        Set<String> add_these_words = this.words(syn_ids);
-                        for (String add_words : add_these_words) {
-                            all_hyponyms.add(add_words);
+                    Set<Integer> allChildrenIDs = this.allSynIDs(synID);
+                    for (int synIDs : allChildrenIDs) {
+                        Set<String> addTheseWords = this.words(synIDs);
+                        for (String addWords : addTheseWords) {
+                            allHyponyms.add(addWords);
                         }
                     }
                 }
             }
         } 
-        return all_hyponyms; 
+        return allHyponyms; 
     }
 
 
     /* Returns ALL the synset IDs (descendents) need to take in, given your original synset ID. */
-    private Set<Integer> allSynIDs(int syn_id) {
+    private Set<Integer> allSynIDs(int synID) {
         Set<Integer> allchildren = new HashSet<Integer>(); 
-        Iterator<int[]> hyp_iter = this.hyponym.iterator();
-        while (hyp_iter.hasNext()) {
-            int[] hyp_array = hyp_iter.next();
-            if (hyp_array[0] == syn_id) {
-                int[] hyp_IDs = Arrays.copyOfRange(hyp_array, 0, hyp_array.length);
-                for (int j = 0; j < hyp_IDs.length; j = j + 1) {
-                    allchildren.add(hyp_IDs[j]);
+        Iterator<int[]> hypIter = this.hyponym.iterator();
+        while (hypIter.hasNext()) {
+            int[] hypArray = hypIter.next();
+            if (hypArray[0] == synID) {
+                int[] hypIDs = Arrays.copyOfRange(hypArray, 0, hypArray.length);
+                for (int j = 0; j < hypIDs.length; j = j + 1) {
+                    allchildren.add(hypIDs[j]);
                 }
                 // Now have to find the subchildren.
-                for (int k = 1; k < hyp_IDs.length; k = k + 1) {
+                for (int k = 1; k < hypIDs.length; k = k + 1) {
                     // Start at 1 because you already found the children of yourself.
-                    Set<Integer> subchildren = this.allSynIDs(hyp_IDs[k]);
+                    Set<Integer> subchildren = this.allSynIDs(hypIDs[k]);
                     // Have to take them all one by one into allchildren.
                     for (Integer subchild : subchildren) {
                         allchildren.add(subchild);
@@ -178,14 +178,14 @@ public class WordNet {
 
 
     /* Returns the words given the syn ID. */
-    private Set<String> words(int syn_id) {
+    private Set<String> words(int synID) {
         Set<String> words = new HashSet<String>();
-        Iterator<String[]> syn_iter2 = this.synset.iterator();
-        while (syn_iter2.hasNext()) {
-            String[] syn_array2 = syn_iter2.next();
-            String[] words2 = syn_array2[1].split(" ");
-            int check_syn_ID = Integer.parseInt(syn_array2[0]);
-            if (check_syn_ID == syn_id) {
+        Iterator<String[]> synIter2 = this.synset.iterator();
+        while (synIter2.hasNext()) {
+            String[] synArray2 = synIter2.next();
+            String[] words2 = synArray2[1].split(" ");
+            int check_synID = Integer.parseInt(synArray2[0]);
+            if (check_synID == synID) {
                 // Found right synset, take in words.
                 for (int k = 0; k < words2.length; k = k + 1) {
                     words.add(words2[k]);
@@ -196,6 +196,9 @@ public class WordNet {
     }
 
 
-}
-
-/* Major thanks to dude at office hours whose name I never figured out (his Macbook said Soham but there's no Soham on the CS61B staff page) who spent 2 hours of his life googling weird java errors and explaining how to fix them to me. */
+/* Major thanks to dude at office hours.
+    * Whose name I never figured out.
+    * His Macbook said Soham but there's no Soham on the CS61B staff page.
+    * He Spent 2 hours of his life googling weird java errors and explaining how to fix them.
+    * Thanks bruh. */
+} 
