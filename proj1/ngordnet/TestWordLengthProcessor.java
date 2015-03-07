7ba2f12fc65a0c6a76e77415163a18829a1c5194
Alice Tarng <atarng@airbears2-10-142-66-226.AirBears2.1918.Berkeley.EDU>
2015-03-06 21:21:45 -0800

    TestWordLengthProcessor written test

--- proj1/ngordnet/TestWordLengthProcessor.java
+++ proj1/ngordnet/TestWordLengthProcessor.java
@@ -1,2 +1,21 @@
 package ngordnet;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestWordLengthProcessor {
+
+    @Test 
+    public void testProcess() {
+        YearlyRecord yr = new YearlyRecord();
+        yr.put("sheep", 100);
+        yr.put("dog", 300);
+        WordLengthProcessor wlp = new WordLengthProcessor();
+        assertEquals(3.5, wlp.process(yr));
+    }
+
+
+    public static void main (String[] args) {
+        jh61b.junit.textui.runClasses(TestWordLengthProcessor.class);
+    }
+}
