e36a51ced0afe7d8eba526a820bd567fd1d52160
Alice Tarng <atarng@airbears2-10-142-66-33.AirBears2.1918.Berkeley.EDU>
2015-03-07 16:04:24 -0800

    YearlyRecord TIME TEST PASSES NOW YESSS

--- proj1/ngordnet/YearlyRecord.java
+++ proj1/ngordnet/YearlyRecord.java
@@ -6,6 +6,8 @@ import java.util.Set;
 import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Arrays;
 
 /* Okay I'm thinking 2 maps? 
  * One for word v. count.
@@ -26,6 +28,7 @@ public class YearlyRecord {
     private Map<String, Integer> countMap;
     private Map<String, Integer> rankMap; 
     private int size;
+    private boolean isRanked = false;
 
     /* Creates a new empty YearlyRecord. */
     public YearlyRecord() {
@@ -68,31 +71,12 @@ public class YearlyRecord {
     public void put(String word, int count) {
         countMap.put(word, count);
         size = size + 1;
-        // Update rankMap as well.
-
-        /* STILL NOT FAST ENOUGH. 
-            * Idea: Somehow sort the elements?
-                * Start from the back and go until find the one that's smaller than you. 
-                * If you're passing other guys, increase their rank.
-                * Take the guy who's smaller than you, and your rank = their rank + 1. 
-                * Maybe this may be faster?, but ain't got time to try right now. Also sorting - I don't think you can sort elements, and writing code that does so will make my put even slower probably. I'll leave it for later. */
 
         // STARTING MODIFICATIONS FOR SPEED
-        int rank = 1;
-        for (String key : rankMap.keySet()) {
-            Integer comparingCount = countMap.get(key);
-            if (comparingCount > count) { // Increase your rank.
-                rank = rank + 1;
-            } else if (comparingCount == count && word.compareTo(key) > 0) {
-                // You're lower alphabetically - increase your rank.
-                rank = rank + 1;
-            } else {
-                // You passed them - increase THEIR rank. 
-                    // Don't think can change directly with get, so gonna just put new pair in.
-                rankMap.put(key, rankMap.get(key) + 1);
-            }
-        }
-        rankMap.put(word, rank);
+
+        /* Don't need to update the rank ALL THE TIME.
+            * Just do it when needed - rank is called. */
+        isRanked = false;
     }
 
 
@@ -110,7 +94,59 @@ public class YearlyRecord {
 
     /* Returns rank of word; most common word = 1. */
     public int rank(String word) {
+        if (isRanked) {
+            return rankMap.get(word);
+        }
+        /*for (String key : countMap.keySet()) {
+            int rank = 1;
+            Integer comparingCount = countMap.get(key);
+            for (String compKey : countMap.keySet()) {
+                Integer currCount = countMap.get(compKey);
+                if (comparingCount < currCount) { // Increase your rank.
+                    rank = rank + 1;
+                } else if (comparingCount == currCount && key.compareTo(compKey) > 0) {
+                    // You're lower alphabetically - increase your rank.
+                    rank = rank + 1;
+                }
+            }
+            rankMap.put(key, rank);
+        }
+        isRanked = true;
+        return rankMap.get(word);*/
+        
+
+        this.buildRankMap();
         return rankMap.get(word);
+
+    }
+
+    /* Helper fxn to build rankMap. */
+    private void buildRankMap() {
+        if (!isRanked) {
+            /* Build rankMap. */
+            /* Dump entires of countMap to array. */
+            String[] wordsArray = countMap.keySet().toArray(new String[0]);
+            // ^ B/c just toArray returns Object[]. Thx to StackOverflow for the trick.*/
+            Arrays.sort(wordsArray, new StringCountComparator());
+            ArrayList<String> wordsArrayList = new ArrayList<String>(Arrays.asList(wordsArray));
+            for (String word2 : wordsArrayList) {
+                rankMap.put(word2, wordsArrayList.indexOf(word2) + 1);
+            }
+            // Need +1 b/c rank starts at 1, not 0.
+        }
+        isRanked = true;
+    }
+
+
+    /* Inner class considered to be part of the containing class --> Full access to all privates both ways.
+        * Access control. */
+    private class StringCountComparator implements Comparator<String> {
+
+        @Override
+        public int compare(String word1, String word2) {
+            // Sort words by descending order of count. 
+            return countMap.get(word2) - countMap.get(word1);
+        }
     }
 
 
@@ -122,38 +158,55 @@ public class YearlyRecord {
             * Iterate through the keys. 
             * If the value = counter, put the key in the list. 
             * Update counter. */
-        List<String> ascendingWords = new ArrayList<String>();
-        Map<String, Integer> rankedmap = this.rankMap;
-        Set<String> keys = rankedmap.keySet();
-        int counter = this.size();
-        while (counter >= 1) {
-            for (String key : keys) {
-                if (counter == rankedmap.get(key)) {
-                    ascendingWords.add(key);
-                }
-            }
-            counter = counter - 1;
+        if (countMap.isEmpty()) {
+            return new ArrayList<String>();
         }
-        return ascendingWords;
+        else if (isRanked) {
+            List<String> ascendingWords = new ArrayList<String>();
+            Map<String, Integer> rankedmap = this.rankMap;
+            Set<String> keys = rankedmap.keySet();
+            int counter = this.size();
+            while (counter >= 1) {
+                for (String key : keys) {
+                    if (counter == rankedmap.get(key)) {
+                        ascendingWords.add(key);
+                    }
+                }
+                counter = counter - 1;
+            }
+            return ascendingWords;
+        }
+
+        // Build the rankMap.
+        this.buildRankMap();
+        return this.words();
     }
 
 
     /* Returns all counts in ascending order of count. */
     public Collection<Number> counts() {
-        List<Number> ascendingCount = new ArrayList<Number>();
-        Map<String, Integer> rankedmap = this.rankMap;
-        Map<String, Integer> countmap = this.countMap;
-        Set<String> keys = rankedmap.keySet();
-        int counter = this.size();
-        while (counter >= 1) {
-            for (String key : keys) {
-                if (counter == rankedmap.get(key)) {
-                    ascendingCount.add(countmap.get(key));
-                }
-            }
-            counter = counter - 1;
+        if (countMap.isEmpty()) {
+            return new ArrayList<Number>();
         }
-        return ascendingCount;
+        else if (isRanked) {
+            List<Number> ascendingCount = new ArrayList<Number>();
+            Map<String, Integer> rankedmap = this.rankMap;
+            Map<String, Integer> countmap = this.countMap;
+            Set<String> keys = rankedmap.keySet();
+            int counter = this.size();
+            while (counter >= 1) {
+                for (String key : keys) {
+                    if (counter == rankedmap.get(key)) {
+                        ascendingCount.add(countmap.get(key));
+                    }
+                }
+                counter = counter - 1;
+            }
+            return ascendingCount;
+        }
+        // Build the rankMap.
+        this.buildRankMap();
+        return this.counts();
     }
 
 
