0d8a7f0b13fe61679022736c914786e6018da827
Alice Tarng <atarng@airbears2-10-142-184-175.AirBears2.1918.Berkeley.EDU>
2015-03-11 16:30:32 -0700

    WordLengthProcessor GOT OVERFLOW SOLVED I THINK YAY

--- proj1/ngordnet/WordLengthProcessor.java
+++ proj1/ngordnet/WordLengthProcessor.java
@@ -5,35 +5,29 @@ public class WordLengthProcessor implements YearlyRecordProcessor {
 
     public double process(YearlyRecord yearlyRecord) {
 
-        /* sum(count * length) / (total counts). */
+        /* sum(count * length) / (total counts). 
+            * So sum((count * length) / (total counts)) should work. 
+            * Doing this b/c want to avoid overflow problems. */
         Collection<String> allWords = yearlyRecord.words();
-        // Using long b/c seems to hold largest possible #s.
-        long totalSum = 0; // Initializing.
+        double avgLength = 0; // Initializing
         long totalCounts = 0;
-        /*long overflows = 0;*/
+        // Going to find totalCounts first, so can use it for distributive magic.
         for (String word : allWords) {
-            int length = word.length();
             int wordCount = yearlyRecord.count(word);
-            totalSum = totalSum + (length * wordCount);
-            /*// In case of overflow.
-            if (totalSum < 0) {
-                overflows = overflows + 1;
-                totalSum = totalSum + 922337203685477580 + 1;
-                // + 1 is for the step to get to the negativeness. 
-            }*/
             totalCounts = totalCounts + wordCount;
         }
 
         if (totalCounts == 0) {
-            // Like no words in the thing I guess.
-            // Doing this to avoid if divide by 0.
+            // No words in the thing (avoid divide by 0).
             return 0;
         }
-        return ((double) totalSum) / ((double) totalCounts);
-        // Can't do division with longs b/c it's integer division - returns int. 
 
-        /* long avgLength = (totalSum + (overflows * 2147483647)) / totalCounts; */
-        /*long avgLength = (totalSum / totalCounts) + ((overflows / totalCounts) * 2147483647);*/
-        // Commented it out b/c compiler doesn't like adding huge #s.
+        for (String word2 : allWords) {
+            int length = word2.length();
+            int wordCount = yearlyRecord.count(word2);
+            avgLength = avgLength + ((double) (wordCount * length) / (double) totalCounts);
+        }
+
+        return avgLength;
     }
 }
