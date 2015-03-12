bff20d8afd4fc2f66fa5bdd1fdfa48e5cb8dea5e
Alice Tarng <atarng@airbears2-10-142-187-171.AirBears2.1918.Berkeley.EDU>
2015-03-12 14:38:39 -0700

    TestAsymptotics.java b/c I'm paranoid

---
+++ lab8/TestAsymptotics.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestAsymptotics {
+
+    @Test
+    public void 
+
+
+
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestYearlyRecord.class);
+    }
+}
