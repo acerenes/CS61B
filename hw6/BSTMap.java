b8678debf6faa286fcd9d79735dcfeea3070b436
Alice Tarng <atarng@airbears2-10-142-186-202.AirBears2.1918.Berkeley.EDU>
2015-03-16 13:05:55 -0700

    BSTMap size, put, get good

--- hw6/BSTMap.java
+++ hw6/BSTMap.java
@@ -1,4 +1,4 @@
-
+import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -20,11 +20,16 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
+    /* All the static-ness needs to match up. 
+        * V & K are non static - can't use a static method on it. 
+        * Non-static type variable cannot be referenced from a static context.
+        * SOMETHING LIKE THAT. 
+        * Also these methods depend on the instance of the class - can't be static. */
     @Override
     public V get(K key) {
         return get(this.root, key);
     }
-    private static V get(Node start, K key) {
+    private V get(Node start, K key) {
         if (start == null) {
             return null;
         }
@@ -43,9 +48,76 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     @Override
     public void put(K key, V value) {
-        SOMEETHTHININIGNIGNING HERREEEEEE
+        if (key == null) {
+            return;
+        }
+        this.root = put(this.root, key, value);
     }
-    private 
+    private Node put(Node start, K key, V value) {
+        // I think you have to return the new "root".
+        if (start == null) {
+            return new Node(key, value, 1);
+        }
+        int cmp = key.compareTo(start.key);
+        if (cmp < 0) {
+            // Move left.
+            start.left = put(start.left, key, value);
+        }
+        else if (cmp > 0) {
+            start.right = put(start.right, key, value);
+        }
+        else {
+            // Update the node.
+            start.value = value;
+        }
+        start.size = 1 + size(start.left) + size(start.right);
+        return start;
+    }
+
+
+
+    @Override
+    public int size() {
+        return size(this.root);
+    }
+    private int size(Node r) {
+        if (r == null) {
+            return 0;
+        }
+        return r.size;
+    }
+
+
+
+    
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    
+
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
 
 
 }
--- hw6/TestBST.java
+++ hw6/TestBST.java
@@ -5,20 +5,20 @@ import static org.junit.Assert.*;
 public class TestBST {
 
     @Test
-    public void testBasic() {
-        BSTMap<String, String> um = new BSTMap<String, String>();
-        um.put("Gracias", "Dios Basado");
-        um.put("The pain", "is so real.");
-        assertEquals("Dios Basado", um.get("Gracias"));
-        assertTrue(um.containsKey("Gracias"));
+    public void testPutGetSize() {
+        BSTMap<Integer, String> um = new BSTMap<Integer, String>();
+        um.put(1, "Dios Basado");
+        um.put(2, "is so real.");
+        assertEquals("Dios Basado", um.get(1));
+        /*assertTrue(um.containsKey("Gracias"));*/
         assertEquals(2 ,um.size());
-        um.clear();
-        assertFalse(um.containsKey("Gracias"));
+        /*um.clear();
+        assertFalse(um.containsKey("Gracias"));*/
     }
 
 
     public static void main(String[] args) {
-        jh61b.junit.textui.runClasses(BSTMap.class);
+        jh61b.junit.textui.runClasses(TestBST.class);
     }
 
 }
