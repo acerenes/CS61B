86e411cf61f65dd361165b9339b5f880f2c5e38b
Alice Tarng <atarng@airbears2-10-142-186-202.AirBears2.1918.Berkeley.EDU>
2015-03-16 13:43:15 -0700

    TestBST.java added test for PrintinOrder

--- hw6/TestBST.java
+++ hw6/TestBST.java
@@ -23,6 +23,19 @@ public class TestBST {
         assertFalse(um.containsKey(1));
     }
 
+    @Test
+    public void testPrintInOrder() {
+        BBSTMap<Integer, String> um = new BSTMap<Integer, String>();
+        um.put(14, "Fourteen");
+        um.put(-5, "Negative Five");
+        um.put(1, "One");
+        um.put(3, "Three");
+        um.put(2, "Two");
+        um.put(-6, "Negative Six");
+        System.out.println("Hey Alice check that the below things print in order of increasing key.");
+        um.printInOrder();
+    }
+
 
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestBST.class);
