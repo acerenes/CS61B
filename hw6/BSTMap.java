4ebe7b1f49b8f584c52cf4deb1e04b5e3d1306b6
Alice Tarng <atarng@airbears2-10-142-66-18.AirBears2.1918.Berkeley.EDU>
2015-03-16 08:36:54 -0700

    BSTMap.java starting to write methods now

--- hw6/BSTMap.java
+++ hw6/BSTMap.java
@@ -1,3 +1,26 @@
 
 
-public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>, 
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private Node root;
+
+
+    private class Node {
+
+        private K key;
+        private V value;
+        private Node left; // Should be default null. 
+        private Node right;
+        private int size;
+
+        private Node(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
+    }
+
+    
+
+
+} 
