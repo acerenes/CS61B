7ed91e0c17195ac9f7145ffd6ed5d576a09bc1bd
Alice Tarng <atarng@airbears2-10-142-186-202.AirBears2.1918.Berkeley.EDU>
2015-03-16 13:09:05 -0700

    TestBST.java added test for clear and contains key

--- hw6/TestBST.java
+++ hw6/TestBST.java
@@ -10,10 +10,17 @@ public class TestBST {
         um.put(1, "Dios Basado");
         um.put(2, "is so real.");
         assertEquals("Dios Basado", um.get(1));
-        /*assertTrue(um.containsKey("Gracias"));*/
         assertEquals(2 ,um.size());
-        /*um.clear();
-        assertFalse(um.containsKey("Gracias"));*/
+    }
+
+    @Test
+    public void testClearContainsKey() {
+        BSTMap<Integer, String> um = new BSTMap<Integer, String>();
+        um.put(1, "Dios Basado");
+        um.put(2, "is so real.");
+        assertTrue(um.containsKey(1));
+        um.clear();
+        assertFalse(um.containsKey(1));
     }
 
 
