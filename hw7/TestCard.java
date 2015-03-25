275cd7a9eb5f5fab43a1ee7a0ddc5673aa4b7f3d
Alice Tarng <atarng@Alices-MacBook-Pro.local>
2015-03-25 14:06:11 -0700

    TestCard.java test for hashCode written

--- hw7/TestCard.java
+++ hw7/TestCard.java
@@ -14,6 +14,21 @@ public class TestCard {
         assertFalse(card3.equals(card4));
     }
 
+    @Test
+    public void testHashCode() {
+        Card card1 = new Card(1, 1);
+        Card card2 = new Card(1, 1);
+        int hash1 = card1.hashCode();
+        int hash2 = card2.hashCode();
+        assertTrue(hash1.equals(hash2));
+
+        Card card3 = new Card(2, 4);
+        Card card4 = new Card(3, 13);
+        int hash3 = card3.hashCode();
+        int hash4 = card4.hashCode();
+        assertFalse(hash3.equals(hash4));
+    }
+
 
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestCard.class);
