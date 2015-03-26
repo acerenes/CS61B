5fff410a680aa9297e66d15a9c4d0bbe6342b4c8
Alice Tarng <atarng@Alices-MacBook-Pro.local>
2015-03-26 10:05:54 -0700

    TestUsernameBank.java start

---
+++ hw7/TestUsernameBank.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestUsernameBank {
+
+
+
+
+
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestUsernameBank.class);
+    }
+}
