4330fed35a46affe773f9a4a88f0119c09ddb4aa
Alice Tarng <atarng@Alices-MacBook-Pro.local>
2015-03-25 20:52:24 -0700

    TestUsername.java wrote tests for the set usernames

--- hw7/TestUsername.java
+++ hw7/TestUsername.java
@@ -9,6 +9,27 @@ public class TestUsername {
         System.out.println((new Username()).usernameString());
     }
 
+    @Test (expected = NullPointerException.class)
+    public void testUsernameNullException() {
+        Username nullUser = new Username(null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testBadLength() {
+        Username badLengthUser = new Username(AA44);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testBadChars() {
+        Username badCharsUser = new Username(A@3);
+    }
+
+    @Test
+    public void testGoodUsername() {
+        System.out.println("Make sure below printed = Aa4");
+        System.out.println((new Username(Aa4)).usernameString());
+    }
+
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestUsername.class);
     }
