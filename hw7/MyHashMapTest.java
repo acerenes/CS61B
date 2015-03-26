13b4a8399e11d212bcf0118daef459ce9daa0d2b
Alice Tarng <atarng@Alices-MacBook-Pro.local>
2015-03-26 12:31:30 -0700

    MyHashMap and MyHashMapTest start

---
+++ hw7/MyHashMapTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyHashMapTest {
+
+
+
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(MyHashMapTest.class);
+    }
+}
