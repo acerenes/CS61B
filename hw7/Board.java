7bee34205a23be32709b829c3304d94539c1952f
Alice Tarng <atarng@Alices-MacBook-Pro.local>
2015-03-27 11:49:24 -0700

    Board.java oh hashcode please just work

--- hw7/Board.java
+++ hw7/Board.java
@@ -46,8 +46,10 @@ public class Board {
         int returnHash = 0;
         for (int i = 0; i < this.pieces.length; i = i + 1) {
             for (int j = 0; j < this.pieces[i].length; j = j + 1) {
+                if (this.pieces[i][j] != null) {
+                    returnHash = returnHash + this.pieces[i][j].hashCode();
+                }
                 returnHash = returnHash << 1;
-                returnHash = returnHash + this.pieces[i][j].hashCode();
             }
         }
         return returnHash;
