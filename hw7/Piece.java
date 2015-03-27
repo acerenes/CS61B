b2048740ff8dbd331fd2b2e83faa20bb070e5ae7
Alice Tarng <atarng@Alices-MacBook-Pro.local>
2015-03-26 22:31:52 -0700

    Piece.java TRYING ALL OF THE SHIFTING NOW

--- hw7/Piece.java
+++ hw7/Piece.java
@@ -81,15 +81,15 @@ public class Piece {
             returnHash = returnHash + 1;
         }
         if (this.isKing) {
-            returnHash = returnHash << 2;
+            returnHash = returnHash << 3;
             returnHash = returnHash + 1;
         }
         if (this.hasCaptured) {
-            returnHash = returnHash << 2;
+            returnHash = returnHash << 3;
             returnHash = returnHash + 1;
         }
         // For x. 
-        returnHash = returnHash << 2;
+        returnHash = returnHash << 3;
         returnHash = returnHash + x;
         // For y.
         returnHash = returnHash << 10;
