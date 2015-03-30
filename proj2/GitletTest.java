2094079befc1a431aae89a309192df856b24178b
Alice Tarng <atarng@airbears2-10-142-184-93.AirBears2.1918.Berkeley.EDU>
2015-03-30 13:43:47 -0700

    GitletTest.java create

---
+++ proj2/GitletTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*; 
+
+public class GitletTest {
+
+
+
+
+
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(GitletTest.class); 
+}
