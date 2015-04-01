646bc4debc4e0f0a0cb5e4139ea0e97c8f4f7b4d
Alice Tarng <atarng@airbears2-10-142-187-50.AirBears2.1918.Berkeley.EDU>
2015-03-31 19:56:24 -0700

    idufhgidsluh

--- proj2/GitletTest.java
+++ proj2/GitletTest.java
@@ -1,5 +1,22 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*; 
+
+/* Taken from GitletPublicTest.java. */
 
 public class GitletTest {
 
@@ -9,4 +26,5 @@ public class GitletTest {
 
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(GitletTest.class); 
+    }
 }
