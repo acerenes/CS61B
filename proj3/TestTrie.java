59aaab9f7ff7d18b4956339914cfc2e9f8ec1af8
Alice Tarng <atarng@Alices-MacBook-Pro.local>
2015-04-20 02:22:07 -0700

    TestTrie.java literally just the test in the spec

---
+++ proj3/TestTrie.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestTrie {
+
+    @Test
+    public void testInsertAndFind() {
+        Trie t = new Trie();
+        t.insert("hello");
+        t.insert("hey");
+        t.insert("goodbye");
+        assertTrue(t.find("hell", false));
+        assertTrue(t.find("hello", true));
+        assertTrue(t.find("good", false));
+        assertFalse(t.find("bye", false));
+        assertFalse(t.find("heyy", false));
+        assertFalse(t.find("hell", true));
+    }
+
+
+    public static void main(String[] args) {
+            jh61b.junit.textui.runClasses(ULLMapTest.class);
+        }
+
+}
